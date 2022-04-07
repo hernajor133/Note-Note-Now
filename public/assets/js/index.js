0a82cddda07281a86af18444219935dae5006040
Jordi <damlabulbul26@gmail.com>
2022-04-06 20:30:16 -0700

    started on js

---
+++ public/assets/js/index.js
@@ -0,0 +1,49 @@
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
+
+if (window.location.pathname === "/notes") {
+  noteTitle = document.querySelector(".note-title");
+  noteText = document.querySelector(".note-texttarea");
+  saveNoteBtn = document.querySelector(".save-note");
+  newNoteBtn = document.querySelector(".new-note");
+  noteList = document.querySelectorAll(".list-container .list-group");
+}
+
+const show = (elem) => {
+  elem.style.display = "inline";
+};
+
+const hide = (elem) => {
+  elem.style.display = "none";
+};
+
+let activeNote = {};
+
+const getNotes = () =>
+  fetch("/api/notes", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+});
+
+const saveNote = (note) =>
+  fetch("/api/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+});
+
+const deleteNote = () =>
+  fetch("/api/notes/${id}", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+});
+
