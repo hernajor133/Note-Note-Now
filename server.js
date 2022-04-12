e3fbc91743ed7ac2d415da6e08586c4f9ed61e64
Jordi <jordihernandez133@gmail.com>
2022-04-12 10:26:40 -0700

    fixed line 9 index.js, extra t letter in note-textarea

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static("public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const noteLog = require("./db/db.json");
 
